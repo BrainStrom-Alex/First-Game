99c3bbbc1eef1d2d2fb3bd60de46b4922db64dcf
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-06-28 14:11:09 +0530

    Add initial Print line

--- main.py
+++ main.py
@@ -20,3 +20,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("You're at a cross road. Where do you want to go?")
+print("     Type 'left' or 'right'" )
