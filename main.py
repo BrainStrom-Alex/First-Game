8140785d41c0641e6bd44a62dbc8c4bf893ec959
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-06-28 14:12:35 +0530

    add initial condition

--- main.py
+++ main.py
@@ -24,3 +24,8 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 print("You're at a cross road. Where do you want to go?")
 print("     Type 'left' or 'right'" )
+condition = input("")
+if condition == "left":
+    print("You've come to a lake. There is an island in the middle of the lake.")
+    print("   Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+    con2 = input("")
